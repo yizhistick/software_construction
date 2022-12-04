5dd5e54848a4416ac82a454f9b0967d85733ff6d
WJC <2360784351@qq.com>
2022-12-04 11:04:09 +0800

    无更新，测试多线程

--- 测试代码/wjc.py
+++ 测试代码/wjc.py
@@ -1,9 +1,35 @@
-dict1 = {"5+2": "7", "3*1": 4, "5*2": 10}
-for key, value in dict1.items():
-    print(eval(key))
-    if str(eval(key)) != value:
-        dict1[key] = False
-    else:
-        dict1[key] = True
+import PySimpleGUI as sg
 
-print(dict1)
+
+import _thread
+import time
+
+# 为线程定义一个函数
+def print_time( threadName, delay):
+   count = 0
+   while count < 5:
+      time.sleep(delay)
+      count += 1
+      print ("%s: %s" % ( threadName, time.ctime(time.time()) ))
+
+
+def prograss():
+    for i in range(1000):
+        sg.one_line_progress_meter(
+            '做题倒计时',
+            i + 1,
+            1000,
+            '进度条走完就要自动交题啦',
+            orientation='h',
+            no_button=True
+        )
+    exit()
+# 创建两个线程
+try:
+   _thread.start_new_thread( print_time, ("Thread-1", 2, ) )
+   _thread.start_new_thread( prograss())
+except:
+   print ("Error: 无法启动线程")
+
+while 1:
+   pass
