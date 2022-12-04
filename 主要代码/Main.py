77b839a4aa8007875cdade051ea14e39fbe7027e
ffxxorz <2273989719@qq.com>
2022-12-04 10:04:18 +0800

    合

--- 主要代码/Main.py
+++ 主要代码/Main.py
@@ -12,11 +12,11 @@ def MainPage():
               [sg.Text('选择年级:'), sg.InputCombo([1, 2, 3, 4, 5, 6], default_value=2), sg.Text("选择题目数量:"),
                sg.InputCombo([10, 20, 40, 60, 80, 100], default_value=40)],
               [sg.Text('请选择下面一个操作：')],
+              [sg.Button('练习', size=20)],
               [sg.Button('限时练习', size=20)],
               [sg.Button('易错题练习', size=20)],
               [sg.Button('导入题目', size=20)],
-              [sg.Button('导出题目', size=20)],
-              [sg.Button('练习', ), sg.Button('退出')]]
+              [sg.Button('退出',size=20)]]
 
     window = sg.Window('小学生口算习题', layout, element_justification="center")
     while True:
@@ -42,7 +42,10 @@ def MainPage():
             ExportPage(op.Operation.Create(values[0], values[1]), errors_list=[])
             window.reappear()
             pass
-        elif event == '开始随机生成题目':
+        elif event == '练习':
+            window.disappear()
+            exercisePage(op.Operation.Create(values[0], values[1]))
+            window.reappear()
             pass
         print('you entered', values)
 
